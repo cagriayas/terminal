c8485360eef7a336baa648c7eaa4e47c24a029bc
Çağrı Ayas <cagriayas@192.168.0.100>
2023-11-19 22:59:02 +0300

    first commit

---
+++ js/commands.js
@@ -0,0 +1,63 @@
+
+
+const error=`<span id="error">Minions cannot understand this command.</span>
+<p>For a list of available commands type <span class="cmd-deco" id="helper">'help'</span></p>`;
+
+const subtext=`<p id="subtxt">For a list of available commands type <span class="cmd-deco" id="helper">'help'</span></p>`;
+
+const help=`
+<table>
+<tr>
+  <td class="cmd-deco">whoami</td>
+  <td>Who is Çağrı?</td>
+</tr>
+<tr>
+  <td class="cmd-deco">experience</td>
+  <td>Work Experience</td>
+</tr>
+<tr>
+  <td class="cmd-deco">skills</td>
+  <td>My skills</td>
+</tr>
+<tr>
+  <td class="cmd-deco">clear</td>
+  <td>Clear the current terminal</td>
+</tr>
+</table>`
+
+const whoami=`
+<div>Hi my name is Çağrı</div>
+<br>
+<div>
+I have been interested in computers and computer management systems for a long time. Currently, I am working hard to become a cloud systems engineer. I am very excited about this. 
+I have no doubt it will be challenging, but my main goal is to become a DevOps engineer. 
+</div>
+`;
+
+
+const experience=`
+<div>
+  <b class="cmd-deco">Bayındır Hastanesi - Help Desk Consultant. from 2018 – Present </b><br>
+    <ul>
+      <li>Creating images and containers for applications using Docker. Managing created containers and images.<br>
+      <li>Deploying applications with high scalability and high availability to Kubernetes with objects such as deployment. Manage objects such as volumes, services.</li>
+      <li>Deployment and management of services such as Microsoft Azure Virtual Machines & App Services and Containers.<br>
+      <li>Deployment and Management of Microsoft Azure LoadBalancer & Application Gateway & Scale Set Services.</li>
+      <li>Azure Cost Management Service<br>
+      <li>And more...</li>
+      <li><br>
+      <li></li>
+      <li><br>
+      <li></li>
+    </ul>
+  <b>Tech Stack:</b> Microsoft Azure, Microsoft Windows, MacOsx, Docker, Kubernetes, Github, Hardware, Office 365 </p>
+</div>`;
+
+const skills=`
+<div>
+  <ul>
+    <li><b>Foreign Language </b>English : Reading Good, Writing Good, Speaking Medium    
+    </li><br>
+  </ul>
+</div>`;
+
